13512619ff615fa477c456950c457aa8382fcfb6
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-03-10 19:07:24 +0000

    added to products models and admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, Category, Review
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review)
