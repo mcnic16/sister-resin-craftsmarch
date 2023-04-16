618c53a6ad9f159daa776fd52c1bd3e594157c83
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-04-16 14:01:07 +0000

    changed aws region name in settings

--- sisterresincrafts/settings.py
+++ sisterresincrafts/settings.py
@@ -180,16 +180,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'sister-resin-crafts'
-    AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
